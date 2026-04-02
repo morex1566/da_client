8f03350018ab2d7734dfdfe4ae9f0f358b0c294b
morex1566 <morex1566@gmail.com>
2026-04-02 10:47:19 +0900

    remove, refactor : 에이전트 파일들은 upper folder에서 처리,  Auth 설정값 추가

--- Assets/Projects/Scripts/Static/Auth.cs
+++ Assets/Projects/Scripts/Static/Auth.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public enum AuthType 
 { 
@@ -15,4 +16,20 @@ public class AuthResult
     public string DisplayName; 
     public string IdToken;    
     public string ErrorMessage;
+}
+
+public static class Auth
+{
+    public static class Google
+    {
+        public static readonly string ClientId = "1037531147924-tomb4bmakelqcuht2igfb9eomegacsso.apps.googleusercontent.com";
+        public static readonly int Port = 5000;
+        public static readonly string RedirectUri = $"http://localhost:{Port}/";
+        public static readonly string EncodedRedirectUri = UnityWebRequest.EscapeURL(RedirectUri);
+        public static readonly string AuthUrl = $"https://accounts.google.com/o/oauth2/v2/auth?" +
+                                                $"client_id={ClientId}&" +
+                                                $"response_type=code&" +
+                                                $"scope=openid%20email%20profile&" +
+                                                $"redirect_uri={EncodedRedirectUri}";
+    }
 }
--- Assets/Projects/Scripts/Static/GoogleAuthServer.cs
+++ Assets/Projects/Scripts/Static/GoogleAuthServer.cs
@@ -1,58 +1,46 @@
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.EventSystems; // UIBehaviour를 위해 필요합니다.
-using UnityEngine.LightTransport;
-using UnityEngine.Networking;
+using UnityEngine.EventSystems;
 
-// UIBehaviour를 상속받고, IAuthService를 구현합니다.
 public class GoogleAuthServicer : UIBehaviour
 {
-    // --- [설정 및 기존 로직은 동일] ---
-    private string clientId = "1037531147924-tomb4bmakelqcuht2igfb9eomegacsso.apps.googleusercontent.com";
-    private string clientSecret = "YOUR_CLIENT_SECRET_HERE";
-    private int authConsolePort = 5000;
+    private HttpListener listener;
 
-    private string redirectUri => $"http://localhost:{authConsolePort}/";
-
-
+    protected override void Awake()
+    {
+        listener = new HttpListener();
+    }
 
     public void AsyncLogin()
     {
-        HttpListener listener = new HttpListener();
-        {
-            listener.Prefixes.Add(redirectUri);
-            listener.Start();
-        }
+        listener.Prefixes.Add(Auth.Google.RedirectUri);
+        listener.Start();
 
-        string encodedRedirectUri = UnityWebRequest.EscapeURL(redirectUri);
-        string authUrl = $"https://accounts.google.com/o/oauth2/v2/auth?" +
-                         $"client_id={clientId}&" +
-                         $"response_type=code&" +
-                         $"scope=openid%20email%20profile&" +
-                         $"redirect_uri={encodedRedirectUri}";
+        Application.OpenURL(Auth.Google.AuthUrl);
 
-        Application.OpenURL(authUrl);
+        listener.GetContextAsync().ContinueWith(OnGetContextCompleted);
+    }
 
-        listener.GetContextAsync().ContinueWith(task => 
-        {
-            var context = task.Result;
-            string code = context.Request.QueryString.Get("code");
+    public void AsyncLogout()
+    {
 
-            // context(브라우저)에 전달할 내용 및 설정
-            byte[] contentBuffer = HTTP.GetCloseTabResponseBuffer();
-            context.Response.ContentLength64 = contentBuffer.Length;
-            context.Response.ContentType = HTTP.ContentType.HTML;
-            context.Response.OutputStream.Write(contentBuffer, 0, contentBuffer.Length);
-            context.Response.OutputStream.Close();
+    }
 
-            listener.Stop();
+    private void OnGetContextCompleted(Task<HttpListenerContext> task)
+    {
+        var context = task.Result;
+        string code = context.Request.QueryString.Get("code");
 
-            Debug.Log("<color=green>로그인 성공! 인증 코드: </color>" + code);
-        });
+        // context(브라우저)에 전달할 내용 및 설정
+        byte[] contentBuffer = HTTP.GetCloseTabResponseBuffer();
+        context.Response.ContentLength64 = contentBuffer.Length;
+        context.Response.ContentType = HTTP.ContentType.HTML;
+        context.Response.OutputStream.Write(contentBuffer, 0, contentBuffer.Length);
+        context.Response.OutputStream.Close();
+
+        listener.Stop();
+
+        Debug.Log("<color=green>로그인 성공! 인증 코드: </color>" + code);
     }
 }
